a299b721052cc7096b312c0c3a118ed56c154053
Chisomo <bandac@oregonstate.edu>
2025-01-25 14:22:49 -0600

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "red-zed"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+*/
 
   required_providers {
     aws = {
